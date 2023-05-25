a1f3c50e72976bd9e41499050d2ef3057d3aa121
Raymoaned <raymond.managuit@outlook.com>
2023-05-25 17:01:02 +0800

    first commit

---
+++ src/components/header/header.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Header(){
+    return(
+        <div className="header">
+            <div className="header-logo">
+                raymond
+            </div>
+            <div className="header-routes">
+                    About
+                    Profile
+                    Contact
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/Hero.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/header/header"
+
+export default function Hero(){
+return(
+  <div className="container">
+    <div className="container-content">
+        <Header/>
+    </div>
+  </div>
+)
+}
